50ff73f425a1ad00d79c9ab543798c8b19f7d641
Bryan Bosire <bryanbosire@gmail.com>
2022-06-08 15:43:00 +0300

    commit tree 4ec4fe389aeeb87b59d883e48912b9a9d05b619f

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//Multiplication
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+//Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 10);
+
+//calculates remainder
+let num3 = 28;
+let num4 = 6;
+const mod = num3 % num4;
+
+//return 20 as highest number in the set
+const max = Math.max(9,10,6,20,19,9)
